7b0107f011ff54521c0e5f3d9a1e6629bc31d1b6
Phillisk <kiragu.w.phillis@gmail.com>
2016-01-19 23:47:04 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+wget http://www.python.org/ftp/python/3.3.5/Python-3.3.5.tar.gz /
+sudo apt-get install python-pip python-pip3 xvfb python-lxml libxml2-dev libxlst1-dev python-dev build-essential python3-dev python3-setuptools /
+python3-numpy python3-scipy libatlas-dev libatlas3gf-base nltk /
+cat info.txt 
